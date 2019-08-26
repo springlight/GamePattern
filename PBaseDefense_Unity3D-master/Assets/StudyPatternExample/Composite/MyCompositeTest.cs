84f06c717b3e05cc2ab2726080d3aada1a39fe6d
springlight <118406814@qq.com>
2019-08-26 18:14:07 +0800

    composite pattern

---
+++ PBaseDefense_Unity3D-master/Assets/StudyPatternExample/Composite/MyCompositeTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCompositeTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        MyIComponent theRoot = new MyComposite("Root");
+        theRoot.Add(new MyLeaf("叶子111"));
+        theRoot.Add(new MyLeaf("叶子222"));
+
+        MyIComponent theChild1 = new MyComposite("子节点1");
+        theChild1.Add(new MyLeaf("子节点1的叶子1"));
+        theChild1.Add(new MyLeaf("子节点2的叶子2"));
+
+        theRoot.Add(theChild1);
+        theRoot.Operation();
+
+        MyLeaf myLeaf = new MyLeaf("111");
+        myLeaf.Add(new MyLeaf("2222"));
+        myLeaf.Operation();
+    }
+	
+	
+}
---
+++ PBaseDefense_Unity3D-master/Assets/StudyPatternExample/Composite/MyIComponent.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MyIComponent
+{
+    protected string m_Value;
+    public abstract void Operation();
+    public virtual void Add(MyIComponent theComponent)
+    {
+        Debug.LogError("子类没有实现Add功能");
+    }
+
+    public virtual void Remove(MyIComponent theComponent)
+    {
+        Debug.LogError("子类没有实现Remove功能");
+    }
+
+    public virtual MyIComponent GetChild(int idx)
+    {
+        Debug.LogError("子类没有实现GetChild功能");
+        return null;
+    }
+
+}
+
+
+public class MyComposite : MyIComponent
+{
+    List<MyIComponent> m_Childs = new List<MyIComponent>();
+    public MyComposite(string value)
+    {
+        m_Value = value;
+    }
+
+    public override void Operation()
+    {
+        Debug.LogError("MyComposite [" + m_Value + "]");
+        foreach(MyIComponent myIComponent in m_Childs)
+        {
+            myIComponent.Operation();
+        }
+
+    }
+
+    public override void Add(MyIComponent theComponent)
+    {
+        m_Childs.Add(theComponent);
+    }
+
+    public override void Remove(MyIComponent theComponent)
+    {
+        m_Childs.Remove(theComponent);
+    }
+
+    public override MyIComponent GetChild(int idx)
+    {
+        return m_Childs[idx];
+    }
+}
+
+public class MyLeaf : MyIComponent
+{
+    public MyLeaf(string value)
+    {
+        m_Value = value;
+    }
+
+    public override void Operation()
+    {
+        Debug.LogError("叶子节点-" + m_Value + "执行Operation");
+    }
+}
